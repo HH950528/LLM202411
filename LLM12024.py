967c6d1080b33c5cde30426d60d4a15a614ff482
HH950528 <keio.acwb@gmail.com>
2024-11-08 06:04:54 +0900

    Update LLM12024.py

--- LLM12024.py
+++ LLM12024.py
@@ -1,11 +1,3 @@
-pip install streamlit
-pip install pyngrok
-pip install openai==0.28
-pip install langchain langchain-community
-pip install openai langchain ipywidgets
-pip install ipywidgets
-
-
 import streamlit as st
 import openai
 
